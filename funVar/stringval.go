13f79033691d865f9d11a0a405f2a0083e630654
daguang.liang <daguang.liang@ankerbox.com>
2019-06-28 18:24:05 +0800

    小写

--- funVar/stringval.go
+++ funVar/stringval.go
@@ -53,7 +53,7 @@ func UnderlineStringToCamel(s string) string {
 	驼峰字符串转成下划线+小些字母
 	例如： 	abcdAccc ==> abcd_accc
 			AbcdA    ==> abcd_a
-	如果遇到有大写字母的地方，则将在前一位将加上下划线 "_", 将大写字母转成小字母（如果是首字母，则只是转成小谢即可）
+	如果遇到有大写字母的地方，则将在前一位将加上下划线 "_", 将大写字母转成小字母（如果是首字母，则只是转成小写即可）
  */
 func CamelStringToUnderline(s string) string {
 	returnString := ""
@@ -61,7 +61,7 @@ func CamelStringToUnderline(s string) string {
 	for i := 0; i <= num; i++ {
 		d := s[i]
 		if d >= 'A' && d <= 'Z' {
-			// 如果遇到有大写字母的地方，则将在前一位将加上下划线 "_", 将大写字母转成小字母（如果是首字母，则只是转成小谢即可）
+			// 如果遇到有大写字母的地方，则将在前一位将加上下划线 "_", 将大写字母转成小字母（如果是首字母，则只是转成小写即可）
 			ordNum := d + 32
 			if i == 0 {
 				returnString = returnString + "" + Stringval(string(ordNum))
